200386aa626b1505e688712f04adf9c822a74250
gaoge <314013991@qq.com>
2019-08-18 21:36:06 +0800

    css处理

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {add, double} from './add'
+import './style/base.css'
 
 console.log(add(1, 2))
 console.log(double(2))
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,32 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
 module.exports = {
-    mode: 'none'
+    output: {
+        filename: 'bundle.js'
+    },
+    mode: 'none',
+    // optimization: {
+    //     minimizer: [
+    //         new OptimizeCSSAssetsPlugin({})
+    //     ]
+    // },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,    // 提取css 插件
+                    // 'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+            // chunkFilename: "[id].css"
+        })
+    ]
 }
