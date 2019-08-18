21634a5039f5d4bc7882f2509e2e92bad272b907
gaoge <314013991@qq.com>
2019-08-18 19:27:38 +0800

    save

---
+++ src/add.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+    return a + b
+}
+
+function double(a) {
+    return a * 2
+}
+
+export { add, double }
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import {add, double} from './add'
+
+console.log(add(1, 2))
+console.log(double(2))
